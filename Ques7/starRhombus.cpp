98c354418c851d0f188d886790bb2d269c6adf02
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-25 03:12:16 +0530

    Add files via upload

---
+++ Ques7/starRhombus.cpp
@@ -0,0 +1,31 @@
+/*
+Print the following pattern
+Sample Input : n = 4
+Output :
+****
+****
+****
+****
+           */
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter row no : ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        //spaces
+        for(int j=1;j<=n-i;j++)
+        {
+          cout<<" "<<"  ";
+        }
+        //stars
+        for(int k=1;k<=n;k++)
+        {
+            cout<<"*"<<"  ";
+        }
+        cout<<endl;
+    }
+}
